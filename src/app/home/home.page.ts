ab2c4df748ecd1b7e3d594945755b5244a9e80e4
gustavovieira <gustavovieira992@gmail.com>
2019-02-18 08:10:49 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
+
+declare var google;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  mapRef = null;
+
+  constructor(
+    private geolocation: Geolocation,
+    private loadingCtrl: LoadingController
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  async loadMap() {
+    const loading = await this.loadingCtrl.create();
+    loading.present();
+    const myLatLng = await this.getLocation();
+    const mapEle: HTMLElement = document.getElementById('map');
+    this.mapRef = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+    google.maps.event
+      .addListenerOnce(this.mapRef, 'idle', () => {
+        loading.dismiss();
+        this.addMaker(myLatLng.lat, myLatLng.lng);
+      });
+  }
+
+  private addMaker(lat: number, lng: number) {
+    const marker = new google.maps.Marker({
+      position: { lat, lng },
+      map: this.mapRef,
+      title: 'Você está aqui!'
+    });
+    this.addInfoWindowToMarker(marker)
+  }
+
+  addInfoWindowToMarker(marker) {
+    var infoWindowContent = '<div id="content"><h1 id="firstHeading" class="firstHeading">' + 'Caesb' + '</h1></div>';
+    var infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+    marker.addListener('click', () => {
+      infoWindow.open(this.mapRef, marker);
+    });
+  }
+
+  private async getLocation() {
+    const rta = await this.geolocation.getCurrentPosition();
+    return {
+      lat: rta.coords.latitude,
+      lng: rta.coords.longitude
+    };
+  }
+
+}
